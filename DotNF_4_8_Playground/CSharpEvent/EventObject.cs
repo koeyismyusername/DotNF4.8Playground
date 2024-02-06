85484a139a0942bcc7127dfa630b7a9180dd82f5
minhyeok Hwang <koey2182@etomato.com>
2024-02-07 08:30:40 +0900

    이벤트 연습 중

---
+++ DotNF_4_8_Playground/CSharpEvent/EventObject.cs
@@ -0,0 +1,24 @@
+﻿using DotNF_4_8_Playground.DesignPattern.ObserverPattern;
+using System;
+
+namespace DotNF_4_8_Playground.CSharpEvent
+{
+    public class EventObject
+    {
+        public delegate void MyEventHandler<TEventArgs>(object sender, TEventArgs e);
+        public event MyEventHandler<IMyObserver> myEvented;
+
+        public event EventHandler<EventArgs> ThresholdReached;
+
+        protected virtual void OnThresholdReached(EventArgs e)
+        {
+            var handler = ThresholdReached;
+            handler?.Invoke(this, e);
+        }
+
+        protected virtual void OnMyEvented(IMyObserver e) { 
+            var handler = myEvented;
+            handler?.Invoke(this, e);
+        }
+    }
+}
---
+++ DotNF_4_8_Playground/Tests/CSharpEventTest.cs
@@ -0,0 +1,19 @@
+﻿using DotNF_4_8_Playground.CSharpEvent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNF_4_8_Playground.Tests
+{
+    public class CSharpEventTest : ITestable
+    {
+        private EventObject eventObject = new EventObject();
+
+        public void Test(TestContext context)
+        {
+            
+        }
+    }
+}
