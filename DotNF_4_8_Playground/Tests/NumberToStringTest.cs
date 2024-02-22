6a84a1ab140560435351a0a526d6a03fdf7438b5
minhyeok Hwang <koey2182@etomato.com>
2024-02-22 09:44:28 +0900

    Feat: Integer를 ToString('#,###')메서드를 사용해 문자열로 변환 시 값이 0이면 빈 문자열이 되는지 확인 테스트 작성

---
+++ DotNF_4_8_Playground/Tests/NumberToStringTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNF_4_8_Playground.Tests
+{
+    public class NumberToStringTest : ITestable
+    {
+        public void Test(TestContext context)
+        {
+            context.EnableStopWatch = false;
+
+            int stockAmount = 0;
+            Console.WriteLine(stockAmount.ToString("#,##0"));
+            Console.WriteLine(stockAmount.ToString("#,###"));
+        }
+    }
+}
