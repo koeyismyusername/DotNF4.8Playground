162870b2543d3971a6fee6d2a27f84bcad8df0c6
minhyeok Hwang <koey2182@etomato.com>
2024-03-06 17:37:17 +0900

    Feat: 정규표현식 테스트 추가

---
+++ DotNF_4_8_Playground/Tests/RegexTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace DotNF_4_8_Playground.Tests
+{
+    internal class RegexTest : ITestable
+    {
+        public void Test(TestContext context)
+        {
+            var durtyStr = "    !!@@##$$%%^^&&**(())__--++==||~~``::;;''??>><<..,,//[[]]{{}}hello 세상 0123";
+            var cleanStr = Regex.Replace(durtyStr, @"[^\w]|_", "");
+            Console.WriteLine(durtyStr);
+            Console.WriteLine(cleanStr);
+        }
+    }
+}
