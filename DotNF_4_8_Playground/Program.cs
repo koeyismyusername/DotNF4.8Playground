abf1f2df9fc4251eec2a044fa4e6a6a478ee2bab
minhyeok Hwang <koey2182@etomato.com>
2024-03-06 15:16:44 +0900

    Feat: ParseIntTest 추가

--- DotNF_4_8_Playground/Program.cs
+++ DotNF_4_8_Playground/Program.cs
@@ -8,7 +8,7 @@ namespace DotNF_4_8_Playground
         {
             var tester = Tester.Instance;
 
-            tester.TestElapsedTime<EnumTest>();
+            tester.TestElapsedTime<ParseIntTest>();
         }
     }
 }
---
+++ DotNF_4_8_Playground/Tests/ParseIntTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNF_4_8_Playground.Tests
+{
+    internal class ParseIntTest : ITestable
+    {
+        public void Test(TestContext context)
+        {
+            context.EnableStopWatch = false;
+
+            var numStr = "      00051035     ";
+            string nullStr = null;
+            var trimedStr = numStr.Trim();
+            Console.WriteLine($"{nameof(numStr)}={numStr}");
+            Console.WriteLine($"{nameof(trimedStr)}={trimedStr}");
+
+            int.TryParse(nullStr, out int numFromNull);
+            int.TryParse(numStr, out int numFromStr);
+            int.TryParse(numStr, out int numFromTrimedStr);
+
+            Console.WriteLine($"{nameof(numFromNull)}={numFromNull}");
+            Console.WriteLine($"{nameof(numFromStr)}={numFromStr}");
+            Console.WriteLine($"{nameof(numFromTrimedStr)}={numFromTrimedStr}");
+
+        }
+    }
+}
